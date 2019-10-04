ee43818b8c1090e1470c158bbdd41ee94ebeeddd
TomRangren <https://github.com/TomRangren/Inl-mninguppg1.git>
2019-10-04 08:42:55 +0200

    fix

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Array_uppgifter1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] v = new int[5];
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Skriv in ett heltal: ");
+                v[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 5 - 1; i >= 0; i--)
+            {
+                Console.WriteLine("Nr: " + v[i]);
+
+            }
+        }
+    }
+}
